4a3c5b8faae768314b802c52b19ebfb18162d66d
Soroar-Mia <miasoroar@gmail.com>
2022-04-18 13:47:58 +0600

    update

--- src/pages/Home/Skill/Skill.js
+++ src/pages/Home/Skill/Skill.js
@@ -8,13 +8,13 @@ const Skill = () => {
             <ol>
                 <li>Hello, dear students; I am Soroar Mia. I am an English teacher, and I have got three years of experience in teaching English. I am here to help you for improving your English with confidence and freedom. It is an excellent platform for learning; you can only judge after receiving and attending my classes. I teach effectively and with full..</li>
                 <br />
-                <li>Well my teaching style can vary depending on the personality of the student. I consider myself to be optimistic and thus can divert the mind of my students into how studying can be fun. The results they'll be getting will be good because of the plans I have for the teaching. What makes me a great teacher? Well I can play guitar too and can teach...</li>
+                <li>Well my teaching style can vary depending on the personality of the student. I consider myself to be optimistic and thus can divert the mind of my students into how studying can be fun. The results they'll be getting will be good because of the plans I have for the teaching. What makes me a great teacher? Well I can play guitar too and can teach..</li>
                 <br />
-                <li>I usually focus on the topic areas which the students find more difficult. Each student learns differently and I adapt my style as needed in order to tailor the sessions to suit individual needs. I review materials assigned by the school and help students to complete homework, solve worksheets, and practice tests to prepare students for weekly...</li>
+                <li>I usually focus on the topic areas which the students find more difficult. Each student learns differently and I adapt my style as needed in order to tailor the sessions to suit individual needs. I review materials assigned by the school and help students to complete homework, solve worksheets, and practice tests to prepare students for weekly..</li>
                 <br />
-                <li>I teach student by Skype. Google Meet. Zoom. LiveBoard But i suggest Skype and student can nock me firs of all and student need biggner level arabic quran teaching book tha's will be strong Foundation and if stay any hafiz brother who forget full quran I will help them for remember the quran .and all people can read small baby Young man old man...</li>
+                <li>I teach student by Skype. Google Meet. Zoom. LiveBoard But i suggest Skype and student can nock me firs of all and student need biggner level arabic quran teaching book tha's will be strong Foundation and if stay any hafiz brother who forget full quran I will help them for remember the quran .and all people can read small baby Young man old man..</li>
                 <br />
-                <li>I am friendly with my students and dedicated and sincere in my job. Classes with me will be fun and very interactive. I always try to comprehend my students' psychology and guide them according to their needs. I always keep a track of my students improvements and weaknesses appreciate them on their achievements etc. Besides, I believe its not...</li>
+                <li>I am friendly with my students and dedicated and sincere in my job. Classes with me will be fun and very interactive. I always try to comprehend my students' psychology and guide them according to their needs. I always keep a track of my students improvements and weaknesses appreciate them on their achievements etc. Besides, I believe its not..</li>
             </ol>
 
         </div>
